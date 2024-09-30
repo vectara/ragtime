db9a92188fa53ea04ce1cf759c81b6007601ccee
adeelehsan <aadeel.ehsan@gmail.com>
2024-09-30 06:27:19 -0700

    whatsapp added

---
+++ whatsapp_bot.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+from flask import Flask, request
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
+from utils import query_vectara
+
+account_sid = os.getenv("ACCOUNT_SID")
+auth_token = os.getenv("Auth_TOKEN")
+twilio_whatsapp_number = os.getenv("TWILIO_WHATSAPP_NUMBER")
+
+
+app = Flask(__name__)
+vectara_prompt = 'vectara-summary-ext-24-05-med-omni'
+
+# Initialize Twilio client
+client = Client(account_sid, auth_token)
+
+
+# Function to send a WhatsApp message via Twilio
+@app.route('/whatsapp', methods=['POST'])
+def whatsapp():
+    logging.info(request.form.get('Body'))
+    incoming_message = request.form.get('Body')
+    user_phone_number = request.form.get('From')
+    vectara_conv_id, response, _ = query_vectara(incoming_message, None, vectara_prompt, None, bot_type="whatsapp")
+
+    logging.info(response)
+
+    # Send the response back to the user with a status callback
+    message = client.messages.create(
+        body=response,
+        from_=twilio_whatsapp_number,
+        to=user_phone_number,
+        # status_callback="https://your-server-url.com/message-status"
+    )
+
+    # Respond to Twilio with an empty response (optional)
+    resp = MessagingResponse()
+    return str(resp)
+
+
+async def start_whatsapp_bot():
+    app.run(debug=True, port=5000)
